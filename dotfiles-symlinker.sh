359ce940ab8caf51708f21c872ca60c014df3555
MidStein <deepakchauhan19feb@gmail.com>
2023-05-25 18:54:49 +0530

    Create script to add symbolic links for dotfiles
    Doesn't symlink .profile for Git Bash because it has .bash_profile

---
+++ dotfiles-symlinker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+dotfiles=$(ls -A ~/dotfiles)
+for dotfile in $dotfiles; do
+  if [[ "$dotfile" != ".git" && "$dotfile" != ".gitignore" ]]; then
+    if [[ !($(uname -s) == MINGW64_NT* && "$dotfile" == ".profile") ]]; then
+      rm ~/"$dotfile"
+      ln -s ~/dotfiles/"$dotfile" ~/"$dotfile"
+    fi
+  fi
+done
+
