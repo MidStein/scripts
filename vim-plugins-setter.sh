ec588122cefb49d2cddd88fcc207ef9024f65e31
MidStein <deepakchauhan19feb@gmail.com>
2023-05-25 20:43:27 +0530

    Change SSH links to HTTPS
    HTTPS will work for any machine

--- vim-plugins-setter.sh
+++ vim-plugins-setter.sh
@@ -3,21 +3,21 @@
 clone='git clone --depth=1'
 
 repositories=(
-  "git@github.com:neoclide/coc.nvim.git"
-  "git@github.com:Yggdroot/indentLine.git"
-  "git@github.com:tpope/vim-commentary.git"
-  "git@github.com:iamcco/markdown-preview.nvim.git"
-  "git@github.com:ctrlpvim/ctrlp.vim.git"
-  "git@github.com:preservim/nerdtree.git"
-  "git@github.com:editorconfig/editorconfig-vim.git"
-  "git@github.com:tpope/vim-surround.git"
-  "git@github.com:mattn/emmet-vim.git"
-  "git@github.com:vim-airline/vim-airline.git"
-  "git@github.com:tpope/vim-fugitive.git"
-  "git@github.com:ryanoasis/vim-devicons.git"
-  "git@github.com:junegunn/fzf.git"
-  "git@github.com:sheerun/vim-polyglot.git"
-  "git@github.com:junegunn/fzf.vim.git"
+  "https://github.com/neoclide/coc.nvim.git"
+  "https://github.com/Yggdroot/indentLine.git"
+  "https://github.com/tpope/vim-commentary.git"
+  "https://github.com/iamcco/markdown-preview.nvim.git"
+  "https://github.com/ctrlpvim/ctrlp.vim.git"
+  "https://github.com/preservim/nerdtree.git"
+  "https://github.com/editorconfig/editorconfig-vim.git"
+  "https://github.com/tpope/vim-surround.git"
+  "https://github.com/mattn/emmet-vim.git"
+  "https://github.com/vim-airline/vim-airline.git"
+  "https://github.com/tpope/vim-fugitive.git"
+  "https://github.com/ryanoasis/vim-devicons.git"
+  "https://github.com/junegunn/fzf.git"
+  "https://github.com/sheerun/vim-polyglot.git"
+  "https://github.com/junegunn/fzf.vim.git"
 )
 
 cd ~/.vim/pack/vendor/start/
