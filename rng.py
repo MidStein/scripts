2af772376ec0591246997839a6fbb75e1fea49b4
MidStein <deepakchauhan19feb@gmail.com>
2023-06-01 10:18:52 +0530

    Add min argument and command-line option
    Felt the need to only print random number of 2 digits, which wasn't
    possible without min argument. Added command-line options to prevent the
    need to enter min, max, count arguments all at once.

---
+++ rng.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import secrets
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--min", type=int)
+parser.add_argument("--max", type=int)
+parser.add_argument("-c", type=int,
+                    help="count of random numbers")
+args = parser.parse_args()
+
+min = args.min if args.min else 1
+count = args.c if args.c else 1
+
+for _ in range(count):
+    print(secrets.randbelow(args.max - min + 1) + min, end=' ')
+
+print()
+
