3801dab10d7b43e3c9f3fc0f8e2e7940f96c6624
MidStein <deepakchauhan19feb@gmail.com>
2024-02-07 21:06:37 +0530

    Use bash loop instead of `find`
    Bash language server would complain when using `find` with `for` (at
    least when `find` was inlined). #bash pointed out that `find -maxdepth
    1` because this could easily solved with a loop. #bash also introduced
    `ln`s `-f` option. Thanks to geirha

--- dotfiles-symlinker.bash
+++ dotfiles-symlinker.bash
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-dotfiles=$(find "$HOME/dotfiles/" -maxdepth 1 -type f)
-for dotfile in $dotfiles; do
-  if [[ "$(basename "$dotfile")" = "init.lua" ]]; then
-    rm "$HOME/.config/nvim/init.lua"
-    ln -s "$dotfile" "$HOME/.config/nvim/init.lua"
-    continue
-  fi
-  rm "$HOME/$(basename "$dotfile")"
-  ln -s "$dotfile" "$HOME/$(basename "$dotfile")"
+set -e
+shopt -s dotglob
+for file in "$HOME"/dotfiles/* ; do
+  [[ -f $file ]] && [[ $(basename "$file") != "init.lua" ]] && [[ $(basename \
+    "$file") != README.md ]] && ln -sf "$file" "$HOME/"
 done
+
+ln -sf "$HOME/dotfiles/init.lua" "$HOME/.config/nvim/"
 
