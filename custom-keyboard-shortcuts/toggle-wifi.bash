5441873bec33d313495bc9c5dba43d4158566dd1
MidStein <deepakchauhan19feb@gmail.com>
2024-05-01 20:33:06 +0530

    add keyboard shortcuts, deal with .config dotfiles

---
+++ custom-keyboard-shortcuts/toggle-wifi.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+if [[ $(nmcli radio wifi) == 'disabled' ]]; then
+  nmcli radio wifi on
+else
+  nmcli radio wifi off
+fi
--- dotfiles-symlinker.bash
+++ dotfiles-symlinker.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
-shopt -s dotglob
-for file in "$HOME"/dotfiles/* ; do
-  [[ -f $file ]] && [[ $(basename "$file") != "init.lua" ]] && [[ $(basename \
-    "$file") != README.md ]] && ln -sf "$file" "$HOME/"
-done
-ln -sf "$HOME/dotfiles/init.lua" "$HOME/.config/nvim/"
 
+for file in "$HOME"/dotfiles/.*; do
+  [[ -f $file ]] && ln -sf "$(realpath "$file")" "$HOME/"
+done
+for dir in "$HOME"/dotfiles/* ; do
+  [[ -d $dir ]] && ln -sf "$(realpath "$dir")" "$HOME/.config"
+done
